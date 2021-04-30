4e6b1feb8d32f6e3251e6376ffb12b70105915d0
MatiasIslaA <matias.isla.a@gmail.com>
2021-04-30 15:55:24 -0400

    Finished project

--- calculator.js
+++ calculator.js
@@ -1,7 +1,13 @@
 let numbers = document.querySelectorAll(".numbers");
 let operators = document.querySelectorAll(".operators");
-let display = document.querySelector(".screen");
+let display = document.querySelector(".display");
 let equal = document.querySelector(".equal");
+let clear = document.querySelector(".clear");
+let changeBackground = document.querySelector(".change");
+let html = document.querySelector("html");
+let randomNumber2 = Math.floor(Math.random() * 10);
+let requestUrl = `https://api.unsplash.com/search/photos?page=${randomNumber2}&query=japan&client_id=ivc6xe9-xNaOJqkN_UfGNrX1sDIqLfHATK74LxFvWW0`;
+
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
@@ -14,6 +20,8 @@ numbers.forEach((number) => {
       display.innerHTML = firstNumber;
     } else {
       secondNumber += e.target.innerText;
+      console.log(firstNumber);
+      console.log(operator);
       console.log(secondNumber);
       display.innerHTML = secondNumber;
     }
@@ -24,21 +32,26 @@ operators.forEach((ope) => {
   ope.addEventListener("click", (e) => {
     if (operator != "" && secondNumber != "") {
       if (operator == "+") {
-        firstNumber = parseInt(firstNumber) + parseInt(secondNumber);
+        firstNumber = parseFloat(firstNumber) + parseFloat(secondNumber);
         display.innerHTML = firstNumber;
         secondNumber = "";
+        operator = e.target.innerText;
       } else if (operator == "-") {
-        firstNumber = parseInt(firstNumber) - parseInt(secondNumber);
+        ola = "-";
+        firstNumber = parseFloat(firstNumber) - parseFloat(secondNumber);
         display.innerHTML = firstNumber;
         secondNumber = "";
+        operator = e.target.innerText;
       } else if (operator == "/") {
-        firstNumber = parseInt(firstNumber) / parseInt(secondNumber);
+        firstNumber = parseFloat(firstNumber) / parseFloat(secondNumber);
         display.innerHTML = firstNumber;
         secondNumber = "";
+        operator = e.target.innerText;
       } else if (operator == "*") {
-        firstNumber = parseInt(firstNumber) * parseInt(secondNumber);
+        firstNumber = parseFloat(firstNumber) * parseFloat(secondNumber);
         display.innerHTML = firstNumber;
         secondNumber = "";
+        operator = e.target.innerText;
       }
     } else {
       operator = e.target.innerText;
@@ -47,8 +60,51 @@ operators.forEach((ope) => {
   });
 });
 
-equal.addEventListener("click", (e) => {
+equal.addEventListener("click", () => {
   if (operator == "+") {
-    display.innerHTML = parseInt(firstNumber) + parseInt(secondNumber);
+    let result = parseFloat(firstNumber) + parseFloat(secondNumber);
+    display.innerHTML = result;
+    firstNumber = result;
+    secondNumber = "";
+    operator = "";
+  } else if (operator == "-") {
+    let result = parseFloat(firstNumber) - parseFloat(secondNumber);
+    display.innerHTML = result;
+    firstNumber = result;
+    secondNumber = "";
+    operator = "";
+  } else if (operator == "*") {
+    let result = parseFloat(firstNumber) * parseFloat(secondNumber);
+    display.innerHTML = result;
+    firstNumber = result;
+    secondNumber = "";
+    operator = "";
+  } else if (operator == "/") {
+    let result = parseFloat(firstNumber) / parseFloat(secondNumber);
+    display.innerHTML = result;
+    firstNumber = result;
+    secondNumber = "";
+    operator = "";
   }
+});
+
+clear.addEventListener("click", () => {
+  display.innerHTML = 0;
+  firstNumber = "";
+  secondNumber = "";
+  operator = "";
+});
+async function getNewImage() {
+  let randomNumber = Math.floor(Math.random() * 10);
+  return fetch(requestUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      let allImages = data.results[randomNumber];
+      return allImages.urls.regular;
+    });
+}
+let randomImage;
+changeBackground.addEventListener("click", async () => {
+  randomImage = await getNewImage();
+  html.style["background-image"] = `url(${randomImage})`;
 });
